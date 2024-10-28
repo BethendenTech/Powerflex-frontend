7817af2530a9b999c0b468afbdf0ab77cd13c5aa
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-10-28 21:46:35 +0530

    “Our system is temporarily offline. Please try again later.”

--- app/internet-not-working/page.tsx
+++ app/internet-not-working/page.tsx
@@ -1,9 +1,21 @@
 "use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Page() {
+    const router = useRouter();
+
+    const handleBack = () => {
+        router.push("/")
+    }
+
     return (
         <div className="pb-[260px] w-full p-[25px] m-auto max-w-[580px] sm:w-full items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
             <h1>Internet Not Working</h1>
-            <p>Please check your internet connection and try again.</p>
+            <p>Our system is temporarily offline. Please try again
+                later.</p>
+
+            <button onClick={() => handleBack()}>Go to home</button>
         </div>
     );
 }
