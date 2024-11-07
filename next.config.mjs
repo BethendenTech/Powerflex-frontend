b1ad4c0a117bc3a53a2fd3be3f8def183ce8441d
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-11-07 16:48:23 +0530

    nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true, // Ignores ESLint errors during the build process
+    },
+};
 
 export default nextConfig;
