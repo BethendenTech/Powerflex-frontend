5486d0162acf4e584458d9cdf1cf2193428745d2
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-11-19 22:12:26 +0530

    fix code

--- components/payment/paymentMethodCard.tsx
+++ components/payment/paymentMethodCard.tsx
@@ -2,6 +2,7 @@
 
 import updateAction from '@/little-state/action';
 import { paymentMethods } from '@/utils/paymentData';
+import { Box, FormControlLabel, List, ListItem, Radio, RadioGroup, Typography } from '@mui/material';
 import { useStateMachine } from 'little-state-machine';
 import React from 'react';
 
@@ -13,40 +14,43 @@ const PaymentMethodCard = () => {
             payment_method: code
         };
         actions.updateAction(formData);
-
-
     }
 
-
     return (
-        <div>
-            <h2 className="font-harmonia text-base font-bold leading-[19.79px] text-left text-black mb-4">Payment Method</h2>
-            <div className="bg-[#ffffff] rounded-[7px]">
-                <ul className="border rounded-lg divide-y divide-gray-200">
-                    {paymentMethods && paymentMethods.map((paymentMethod) => {
-                        return (
-                            <li
-                                key={``}
-                                className="p-4 cursor-pointer flex items-center justify-between"
+        <Box>
+            <Typography variant="h6" fontWeight="bold">
+                Payment Method
+            </Typography>
+
+            <List
+            >
+                <RadioGroup
+                    name="payment"
+                    value={state.payment_method}
+                    onChange={(event) => onChangeMethod(event.target.value)}
+                >
+                    {paymentMethods &&
+                        paymentMethods.map((paymentMethod) => (
+                            <ListItem
+                                key={paymentMethod.value}
+                               
                                 onClick={() => onChangeMethod(paymentMethod.value)}
                             >
-                                <label className="flex items-center">
-                                    <input
-                                        type="radio"
-                                        name="payment"
-                                        value={paymentMethod.value}
-                                        checked={state.payment_method === paymentMethod.value}
-                                        onChange={() => onChangeMethod(paymentMethod.value)}
-                                        className="form-radio h-5 w-5 text-blue-600"
-                                    />
-                                    <span className="label font-bold ml-3 text-lg">{paymentMethod.label}</span>
-                                </label>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
-        </div>
+                                <FormControlLabel
+                                    value={paymentMethod.value}
+                                    control={<Radio />}
+                                    label={
+                                        <Typography variant="body1" fontWeight="bold">
+                                            {paymentMethod.label}
+                                        </Typography>
+                                    }
+                                    sx={{ margin: 0 }}
+                                />
+                            </ListItem>
+                        ))}
+                </RadioGroup>
+            </List>
+        </Box>
     );
 };
 
--- components/payment/paymentSummary.tsx
+++ components/payment/paymentSummary.tsx
@@ -2,7 +2,7 @@
 
 import { QuoteInterface } from "@/types/quotation";
 import { renderNaira } from "@/utils/currency";
-import { Box, Card, CardContent, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow, Typography } from "@mui/material";
+import { Box, Card, CardContent, CardHeader, Table, TableBody, TableCell, TableFooter, TableHead, TableRow, Typography } from "@mui/material";
 
 interface ComponentProps {
     quote: QuoteInterface;
@@ -10,62 +10,59 @@ interface ComponentProps {
 
 const PaymentSummaryCard = ({ quote }: ComponentProps) => {
     return (
-        <Box mb={2}>
-            <Typography textAlign="left" variant="h5" component="h5">
-                Outright Purchase
-            </Typography>
-
-            <Card>
-                <CardContent>
+        <Card sx={{ mt: 2, mb: 2 }}>
+            <CardHeader
+                title="Outright Purchase"
+            />
+            <CardContent>
 
 
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell colSpan={2} align="left">Equipment</TableCell>
-                                <TableCell colSpan={2} align="right">{quote?.total_equipments}</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            <TableRow>
-                                <TableCell></TableCell>
-                                <TableCell align="left">Solar panels</TableCell>
-                                <TableCell align="right">{quote?.number_of_panels}</TableCell>
-                                <TableCell></TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell></TableCell>
-                                <TableCell align="left">Inverters</TableCell>
-                                <TableCell align="right">{quote?.number_of_inverters}</TableCell>
-                                <TableCell></TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell></TableCell>
-                                <TableCell align="left">Batteries</TableCell>
-                                <TableCell align="right">{quote?.number_of_batteries}</TableCell>
-                                <TableCell></TableCell>
-                            </TableRow>
-                        </TableBody>
-                        <TableFooter>
-                            <TableRow>
-                                <TableCell colSpan={2} align="left">Installation</TableCell>
-                                <TableCell colSpan={2} align="right">{renderNaira(quote?.installer_cost)}</TableCell>
-                            </TableRow>
-                        </TableFooter>
-                    </Table>
+                <Table sx={{border:"none"}}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell colSpan={2} align="left">Equipment</TableCell>
+                            <TableCell colSpan={2} align="right">{quote?.total_equipments}</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell></TableCell>
+                            <TableCell align="left">Solar panels</TableCell>
+                            <TableCell align="right">{quote?.number_of_panels}</TableCell>
+                            <TableCell></TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell></TableCell>
+                            <TableCell align="left">Inverters</TableCell>
+                            <TableCell align="right">{quote?.number_of_inverters}</TableCell>
+                            <TableCell></TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell></TableCell>
+                            <TableCell align="left">Batteries</TableCell>
+                            <TableCell align="right">{quote?.number_of_batteries}</TableCell>
+                            <TableCell></TableCell>
+                        </TableRow>
+                    </TableBody>
+                    <TableFooter>
+                        <TableRow>
+                            <TableCell colSpan={2} align="left">Installation</TableCell>
+                            <TableCell colSpan={2} align="right">{renderNaira(quote?.installer_cost)}</TableCell>
+                        </TableRow>
+                    </TableFooter>
+                </Table>
 
-                    <Box textAlign="center">
-                        <Typography variant="h5" component="h5">
-                            Total Cost
-                        </Typography>
-                        <Typography variant="h5" component="h5">
-                            {renderNaira(quote?.total_cost_naira)}
-                        </Typography>
-                    </Box>
+                <Box textAlign="center">
+                    <Typography variant="h5" component="h5">
+                        Total Cost
+                    </Typography>
+                    <Typography variant="h5" component="h5">
+                        {renderNaira(quote?.total_cost_naira)}
+                    </Typography>
+                </Box>
 
-                </CardContent>
-            </Card>
-        </Box>
+            </CardContent>
+        </Card>
     )
 }
 
