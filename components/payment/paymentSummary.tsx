cb52c1c8ef150bb1bf7cb87f51b872c88427f9bc
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-11-19 21:51:58 +0530

    fix payment table

--- components/payment/paymentSummary.tsx
+++ components/payment/paymentSummary.tsx
@@ -2,7 +2,7 @@
 
 import { QuoteInterface } from "@/types/quotation";
 import { renderNaira } from "@/utils/currency";
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import { Box, Card, CardContent, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow, Typography } from "@mui/material";
 
 interface ComponentProps {
     quote: QuoteInterface;
@@ -19,40 +19,40 @@ const PaymentSummaryCard = ({ quote }: ComponentProps) => {
                 <CardContent>
 
 
-                    <table className="table-auto w-full">
-                        <tbody>
-                            <tr>
-                                <td className="sub-content text-left">Equipment</td>
-                                <th className="sub-content text-right">{quote?.total_equipments}</th>
-                            </tr>
-                        </tbody>
-                    </table>
-
-                    <table className="table-auto w-full">
-                        <tbody>
-                            <tr className='pl-4 flex justify-around'>
-                                <td className="sub-content text-left">Solar panels</td>
-                                <th className="sub-content text-left">{quote?.number_of_panels}</th>
-                            </tr>
-                            <tr className='pl-4 flex justify-around'>
-                                <td className="sub-content text-left">Inverters</td>
-                                <th className="sub-content text-left">{quote?.number_of_inverters}</th>
-                            </tr>
-                            <tr className='pl-4 flex justify-around'>
-                                <td className="sub-content text-left">Batteries</td>
-                                <th className="sub-content text-left">{quote?.number_of_batteries}</th>
-                            </tr>
-                        </tbody>
-                    </table>
-
-                    <table className="table-auto w-full">
-                        <tbody>
-                            <tr>
-                                <td className="sub-content text-left">Installation</td>
-                                <th className="sub-content text-right">{renderNaira(quote?.installer_cost)}</th>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell colSpan={2} align="left">Equipment</TableCell>
+                                <TableCell colSpan={2} align="right">{quote?.total_equipments}</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell></TableCell>
+                                <TableCell align="left">Solar panels</TableCell>
+                                <TableCell align="right">{quote?.number_of_panels}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell></TableCell>
+                                <TableCell align="left">Inverters</TableCell>
+                                <TableCell align="right">{quote?.number_of_inverters}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell></TableCell>
+                                <TableCell align="left">Batteries</TableCell>
+                                <TableCell align="right">{quote?.number_of_batteries}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                        </TableBody>
+                        <TableFooter>
+                            <TableRow>
+                                <TableCell colSpan={2} align="left">Installation</TableCell>
+                                <TableCell colSpan={2} align="right">{renderNaira(quote?.installer_cost)}</TableCell>
+                            </TableRow>
+                        </TableFooter>
+                    </Table>
 
                     <Box textAlign="center">
                         <Typography variant="h5" component="h5">
