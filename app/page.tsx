a958b6bfd24c8343a9bba2cb21013a9bdbabd91e
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-11-25 16:19:15 +0530

    Apply drop shadow

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import { Box, Button, Container, Grid2, Stack, Typography } from "@mui/material";
-import Image from "next/image";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import Link from "next/link";
 
 export default function Home() {
@@ -7,9 +6,41 @@ export default function Home() {
     <Box>
 
       <Box textAlign="center" mt={30} mb={20}>
-        <Typography variant="h2" component="h1" fontWeight="bold" fontSize={36} color="#fff">Powering your world</Typography>
-        <Typography variant="h2" component="h2" fontWeight="bold" fontSize={33} color="#fff">with clean energy solutions</Typography>
-        <Typography color="#fff">Affordable, reliable and tailored solar solutions for every home and business</Typography>
+        <Typography
+          variant="h2"
+          component="h1"
+          fontWeight="bold"
+          fontSize={36}
+          color="#fff"
+          sx={{
+            textShadow: '2px 2px 5px #000',
+          }}
+        >
+          Powering your world
+        </Typography>
+
+        <Typography
+          variant="h2"
+          component="h2"
+          fontWeight="bold"
+          fontSize={33}
+          color="#fff"
+          sx={{
+            textShadow: '2px 2px 5px #000',
+          }}
+        >
+          with clean energy solutions
+        </Typography>
+
+        <Typography
+          color="#fff"
+          mt={4}
+          sx={{
+            textShadow: '1px 1px 5px #000',
+          }}
+        >
+          Affordable, reliable and tailored solar solutions for every home and business
+        </Typography>
       </Box>
 
 
@@ -30,8 +61,6 @@ export default function Home() {
             Get a quote
           </Button>
         </Stack>
-
-
       </Box>
 
     </Box>
