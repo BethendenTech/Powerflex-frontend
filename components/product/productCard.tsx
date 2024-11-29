12be4e9a15f20a07aa81b40464088eb489bcb822
Tauqirsheikh <mdsheikhtouqeer2000@gmail.com>
2024-11-29 22:39:27 +0530

    fix product cards

--- components/product/productCard.tsx
+++ components/product/productCard.tsx
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Button, Grid2 } from '@mui/material';
+import { Box, Button } from '@mui/material';
 
 type ProductDataType = {
     title: string;
@@ -14,16 +14,30 @@ type ProductDataType = {
 const ProductCard: React.FC<ProductDataType> = ({ title, description, image }) => {
 
     return (
-        <Card sx={{
-            mt: 2,
-            height: '100%',
-            borderRadius: '12px',
-            backgroundColor: 'rgba(255, 255, 255, 0.7)',
-            border: '1px solid #ccc',
-        }}>
-            <CardContent sx={{ padding: '15px' }}>
-                <Grid2 container spacing={2}>
-                    <Grid2 size={{ xs: 12, md: 6 }}>
+        <>
+            <Card sx={{
+                display: 'flex',
+                mt: 2,
+                padding: {
+                    lg: '0px 12px',
+                    md: '0px 12px',
+                    sm: '0px 0px',
+                    xs: '0px 0px',
+                },
+                height: '100%',
+                borderRadius: '12px',
+                backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                border: '1px solid #ccc',
+                alignItems: 'center',
+                flexDirection: {
+                    lg: 'row',
+                    md: 'row',
+                    sm: 'column',
+                    xs: 'column'
+                }
+            }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <CardContent sx={{ flex: '1 0 auto', p: 0 }}>
                         <Typography component="div" variant="h6" fontSize={26} color='#191919' fontWeight='bold'>
                             {title}
                         </Typography>
@@ -34,21 +48,24 @@ const ProductCard: React.FC<ProductDataType> = ({ title, description, image }) =
                         >
                             {description}
                         </Typography>
-
-                        <Button variant='outlined' color='secondary' sx={{ mt: 5, width: 200 }}>Learn More</Button>
-                    </Grid2>
-                    <Grid2 size={{ xs: 12, md: 6 }}>
-                        <CardMedia
-                            component="img"
-                            sx={{ width: "100%", height: 200, borderRadius: '12px' }}
-                            image={image}
-                            alt={title}
-                        />
-                    </Grid2>
-                </Grid2>
-            </CardContent>
-
-        </Card>
+                    </CardContent>
+                    <Box sx={{ display: 'flex', alignItems: 'center', pb: 1 }}>
+                        <Button
+                            variant='outlined'
+                            sx={{ mt: 3, width: 200, color: '#595959', borderColor: '#595959' }}>
+                            Learn More</Button>
+                    </Box>
+                </Box>
+                <Box p={1} sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <CardMedia
+                        component="img"
+                        sx={{ width: "100%", height: 'auto', borderRadius: '12px' }}
+                        image={image}
+                        alt={title}
+                    />
+                </Box>
+            </Card>
+        </>
     );
 }
 
