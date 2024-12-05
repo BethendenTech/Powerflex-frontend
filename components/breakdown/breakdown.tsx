d5029e1a621cd423c14009e9f365ba62ea01951a
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-12-06 00:26:58 +0530

    dd

--- components/breakdown/breakdown.tsx
+++ components/breakdown/breakdown.tsx
@@ -51,7 +51,7 @@ export default function Breakdown({ breakdowns, register, setValue }: BreakdownP
             <TableBody>
                 <TableRow>
                     <TableCell>
-                        {breakdowns[index]?.id}
+                        
                         <FormControlLabel
                             control={
                                 <Checkbox
