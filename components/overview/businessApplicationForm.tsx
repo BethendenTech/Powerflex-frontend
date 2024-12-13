2de9bb74dd6abdb5592fdc0218b92265a38edb6b
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-12-13 23:23:26 +0530

    update ip

--- components/overview/businessApplicationForm.tsx
+++ components/overview/businessApplicationForm.tsx
@@ -16,33 +16,43 @@ const BusinessApplicationForm = () => {
     // Set up react-hook-form with default values for the inputs
     const { control, register, handleSubmit, setValue, formState: { errors }, watch } = useForm({
         defaultValues: {
-            role: 'Director',
-            other_role: '',
-            first_name: '',
-            last_name: '',
-            phone_number: '',
-            email: '',
-            business_name: '',
-            house_number: '',
-            street_name: '',
-            nearest_bus_stop: '',
-            lga: '',
-            state: '',
-            bvn: '',
+            application_type: "",
+            bvn: "",
+            other_role: "",
+            first_name: "",
+            last_name: "",
+            house_number: "",
+            street_address: "",
+            landmark: "",
+            bus_stop: "",
+            occupation: "",
+            business_role: "",
+            business_name: "",
+            business_address: "",
+            town: "",
+            city: "",
+            state: "",
+            lga: "",
+            email: "",
+            phone_number: "",
+            reference_phone1: "",
+            reference_phone2: "",
+            how_heard_about: "",
             applicant_id_card: "",
             company_registration_document: "",
             bank_statements: "",
-            recent_utility_bill: ""
+            recent_utility_bill: "",
         }
     });
 
 
-    const role = watch("role");
+    const business_role = watch("business_role");
 
     // Handle form submission
     const onSubmit = async (formData: any) => {
+        formData["application_type"] = "business"
         formData["quote_number"] = state.quote_number
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/apply-business/`, {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/cerate-quote-application/`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -51,6 +61,27 @@ const BusinessApplicationForm = () => {
         });
 
         if (response.ok) {
+            formData["firstname"] = formData["first_name"]
+            formData["lastname"] = formData["last_name"]
+            formData["application_channel"] = "powerflex"
+            formData["device_id"] = "1856"
+            formData["quantity"] = "1"
+            formData["device_price"] = "1"
+            formData["sentinel_sld"] = "no"
+            formData["sentinel_sap"] = "no"
+
+
+            const response = await fetch(`${process.env.NEXT_PUBLIC_FINANCE_BASE_URL}/finance/prod/checkout/application/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'requestApiKey': "jOwhFfcbHGaIkTUAKo8rPSMBp3xd"
+                },
+                body: JSON.stringify(formData),
+            });
+
+            console.log("response", response)
+
             router.push(`/quotation/application-success`);
         }
     };
@@ -65,23 +96,23 @@ const BusinessApplicationForm = () => {
 
                 <FormControl
                     fullWidth
-                    error={!!errors.role}
+                    error={!!errors.business_role}
                 >
                     <FormLabel>
-                        Role
+                        Business_role
                     </FormLabel>
                     <Select
                         type='text'
-                        {...register("role", { required: "Role is required" })}
+                        {...register("business_role", { required: "Role is required" })}
                     >
                         <MenuItem value="director">Director</MenuItem>
                         <MenuItem value="other">Other</MenuItem>
                     </Select>
-                    <FormHelperText>{errors?.role?.message}</FormHelperText>
+                    <FormHelperText>{errors?.business_role?.message}</FormHelperText>
                 </FormControl>
 
 
-                {role == "other" && <FormControl
+                {business_role == "other" && <FormControl
                     fullWidth
                     error={!!errors.other_role}
                 >
@@ -140,6 +171,34 @@ const BusinessApplicationForm = () => {
                     <FormHelperText>{errors?.phone_number?.message}</FormHelperText>
                 </FormControl>
 
+                <FormControl
+                    fullWidth
+                    error={!!errors.reference_phone1}
+                >
+                    <FormLabel>
+                        Reference Phone 1
+                    </FormLabel>
+                    <OutlinedInput
+                        type='text'
+                        {...register("reference_phone1", { required: "Reference Phone 1 is required" })}
+                    />
+                    <FormHelperText>{errors?.reference_phone1?.message}</FormHelperText>
+                </FormControl>
+
+                <FormControl
+                    fullWidth
+                    error={!!errors.reference_phone2}
+                >
+                    <FormLabel>
+                        Reference Phone 2
+                    </FormLabel>
+                    <OutlinedInput
+                        type='text'
+                        {...register("reference_phone2", { required: "Reference Phone 2 is required" })}
+                    />
+                    <FormHelperText>{errors?.reference_phone2?.message}</FormHelperText>
+                </FormControl>
+
 
                 <FormControl
                     fullWidth
@@ -160,6 +219,19 @@ const BusinessApplicationForm = () => {
 
 
 
+                <FormControl
+                    fullWidth
+                    error={!!errors.house_number}
+                >
+                    <FormLabel>
+                        House Number
+                    </FormLabel>
+                    <OutlinedInput
+                        type='text'
+                        {...register("house_number", { required: "House Number is required" })}
+                    />
+                    <FormHelperText>{errors?.house_number?.message}</FormHelperText>
+                </FormControl>
                 <FormControl
                     fullWidth
                     error={!!errors.business_name}
@@ -193,31 +265,113 @@ const BusinessApplicationForm = () => {
 
                 <FormControl
                     fullWidth
-                    error={!!errors.street_name}
+                    error={!!errors.street_address}
                 >
                     <FormLabel>
-                        Street Name
+                        Street Address
                     </FormLabel>
                     <OutlinedInput
                         type='text'
-                        {...register("street_name", { required: "Street Name is required" })}
+                        {...register("street_address", { required: "Street Address is required" })}
                     />
-                    <FormHelperText>{errors?.street_name?.message}</FormHelperText>
+                    <FormHelperText>{errors?.street_address?.message}</FormHelperText>
+                </FormControl>
+                <FormControl
+                    fullWidth
+                    error={!!errors.landmark}
+                >
+                    <FormLabel>
+                        Landmark
+                    </FormLabel>
+                    <OutlinedInput
+                        type='text'
+                        {...register("landmark", { required: "Landmark is required" })}
+                    />
+                    <FormHelperText>{errors?.landmark?.message}</FormHelperText>
                 </FormControl>
 
 
                 <FormControl
                     fullWidth
-                    error={!!errors.nearest_bus_stop}
+                    error={!!errors.bus_stop}
                 >
                     <FormLabel>
-                        Nearest Bus Stop
+                        Bus Stop
                     </FormLabel>
                     <OutlinedInput
                         type='text'
-                        {...register("nearest_bus_stop", { required: "Nearest Bus Stop is required" })}
+                        {...register("bus_stop", { required: "Bus Stop is required" })}
                     />
-                    <FormHelperText>{errors?.nearest_bus_stop?.message}</FormHelperText>
+                    <FormHelperText>{errors?.bus_stop?.message}</FormHelperText>
+                </FormControl>
+
+                <FormControl
+                    fullWidth
+                    error={!!errors.occupation}
+                >
+                    <FormLabel>
+                        Occupation
+                    </FormLabel>
+                    <OutlinedInput
+                        type='text'
+                        {...register("occupation", { required: "Occupation is required" })}
+                    />
+                    <FormHelperText>{errors?.occupation?.message}</FormHelperText>
+                </FormControl>
+
+                <FormControl
+                    fullWidth
+                    error={!!errors.business_name}
+                >
+                    <FormLabel>
+                        Business Name
+                    </FormLabel>
+                    <OutlinedInput
+                        type='text'
+                        {...register("business_name", { required: "Business Name is required" })}
+                    />
+                    <FormHelperText>{errors?.business_name?.message}</FormHelperText>
+                </FormControl>
+
+                <FormControl
+                    fullWidth
+                    error={!!errors.business_address}
+                >
+                    <FormLabel>
+                        Business Address
+                    </FormLabel>
+                    <OutlinedInput
+                        type='text'
+                        {...register("business_address", { required: "Business Address is required" })}
+                    />
+                    <FormHelperText>{errors?.business_address?.message}</FormHelperText>
+                </FormControl>
+
+                <FormControl
+                    fullWidth
+                    error={!!errors.town}
+                >
+                    <FormLabel>
+                        Town
+                    </FormLabel>
+                    <OutlinedInput
+                        type='text'
+                        {...register("town", { required: "Town is required" })}
+                    />
+                    <FormHelperText>{errors?.town?.message}</FormHelperText>
+                </FormControl>
+                <FormControl
+                    fullWidth
+                    error={!!errors.city}
+                >
+                    <FormLabel>
+                        City
+                    </FormLabel>
+                    <OutlinedInput
+                        type='text'
+                        {...register("city", { required: "City is required" })}
+                    />
+                    <FormHelperText>{errors?.city?.message}</FormHelperText>
                 </FormControl>
 
                 <SelectStateComponent
