0cee2bba12d56448090ec2fa8488217dba48cb0f
Touqeer Sheikh <mdsheikhtouqeer2000@gmail.com>
2025-01-10 23:05:29 +0530

    create terns and condiiton popup

--- app/quotation/details/page.tsx
+++ app/quotation/details/page.tsx
@@ -6,10 +6,10 @@ import { useForm } from 'react-hook-form';
 import { useStateMachine } from "little-state-machine";
 import updateAction from '@/little-state/action';
 import React, { useContext, useEffect, useState } from 'react';
-import { Box, Button, OutlinedInput } from '@mui/material';
+import { Box, Button, Checkbox, FormControlLabel, OutlinedInput } from '@mui/material';
 import CustomizedSteppers from '@/components/stepper';
 import { NextButton } from '@/components/button/style';
-import { FormInputField, FormTitle, FormValidation } from '@/components/form/style';
+import { Description, FormInputField, FormTitle, FormValidation, Heading, Title } from '@/components/form/style';
 import { DialogContext } from '@/contexts/dialogContext';
 
 export default function Page() {
@@ -26,6 +26,7 @@ export default function Page() {
       email: "",
       phone_number: "",
       is_finance: false,
+      agree_terms: false,
     }
   });
 
@@ -98,35 +99,101 @@ export default function Page() {
     getTermsData();
   }, []);
 
-  useEffect(() => {
-    if (!state?.terms_and_conditions) {
-      if (termsData) {
-        openDialog({
-          title: "Terms and Conditions",
-          content: (
-            <Box>
+  // useEffect(() => {
+  //   if (!state?.terms_and_conditions) {
+  //     if (termsData) {
+  //       openDialog({
+  //         title: "Terms and Conditions",
+  //         content: (
+  //           <Box>
 
-              <p>{termsData}</p>
-            </Box>
-          ),
-          actions: (
-            <Box>
-              <Button
-                variant='contained'
-                onClick={() => {
-                  actions.updateAction({ "terms_and_conditions": true });
-                  closeDialog();
-                }}
-              >
-                Accept
-              </Button>
+  //             <p>{termsData}</p>
+  //           </Box>
+  //         ),
+  //         actions: (
+  //           <Box>
+  //             <Button
+  //               variant='contained'
+  //               onClick={() => {
+  //                 actions.updateAction({ "terms_and_conditions": true });
+  //                 closeDialog();
+  //               }}
+  //             >
+  //               Accept
+  //             </Button>
 
-            </Box>
-          )
-        });
-      }
-    }
-  }, [state?.terms_and_conditions, termsData])
+  //           </Box>
+  //         )
+  //       });
+  //     }
+  //   }
+  // }, [state?.terms_and_conditions, termsData])
+
+  const openTermsPopup = () => {
+    openDialog({
+      title: (
+        <Box>
+          <Title sx={{ fontSize: "20px" }}>
+            Powerflex Energy Calculator - Terms and Conditions
+          </Title>
+        </Box>
+      ),
+      content: (
+        <Box>
+          <Heading sx={{ fontSize: "18px", color: "#424242", mb: 2, textAlign: "left" }}>
+            Welcome to Powerflex! Please review the terms before proceeding:
+          </Heading>
+          <ul style={{
+            margin: 0,
+            paddingLeft: "1.2em",
+            listStyleType: "disc",
+            fontFamily: "'Harmonia Sans Pro', sans-serif",
+            fontWeight: 400,
+            fontSize: "18px",
+            color: "#424242",
+          }}>
+            <li style={{ fontSize: "0.9em", lineHeight: "1.5", paddingBottom: "10px" }}>
+              <b>Estimate Purpose:</b> The calculator provides a <b>final estimate</b> of energy system requirements and costs basedon your provided data.
+            </li>
+            <li style={{ fontSize: "0.9em", lineHeight: "1.5", paddingBottom: "10px" }}>
+              <b>Data Accuracy:</b> Estimates rely on the <b>accuracy of your provided data</b>
+              (usage, location, preferences).
+            </li>
+            <li style={{ fontSize: "0.9em", lineHeight: "1.5", paddingBottom: "10px" }}>
+              <b>Factors Affecting Implementation:</b> Location, energy patterns, cabling, installation complexity,, and site-specific findings may affect system setup.
+            </li>
+            <li style={{ fontSize: "0.9em", lineHeight: "1.5", paddingBottom: "10px" }}>
+              <b>Estimate Validity:</b> The estimate is <b>final</b>, but adjustments may arise after a detailed <b>site assessment</b>.
+            </li>
+            <li style={{ fontSize: "0.9em", lineHeight: "1.5", paddingBottom: "10px" }}>
+              <b>Financing:</b> Approval depends on <b>eligibity checks</b> and may require additional documentation.
+            </li>
+            <li style={{ fontSize: "0.9em", lineHeight: "1.5", paddingBottom: "10px" }}>
+              <b>Privacy:</b> Your data is securely processed per our <b>Privacy Policy</b>
+            </li>
+            <li style={{ fontSize: "0.9em", lineHeight: "1.5", paddingBottom: "10px" }}>
+              <b>User Agreement:</b> By proceeding, you confirm your {`data's`} accuracy, understand adjustment may occur after.
+            </li>
+          </ul>
+          <Description sx={{ fontSize: "18px", textAlign: 'left' }}>{termsData}</Description>
+        </Box>
+      ),
+      actions: (
+        <Box>
+          <Button
+            sx={{ padding: "2px 10px" }}
+            variant='contained'
+            onClick={() => {
+              actions.updateAction({ "terms_and_conditions": true });
+              closeDialog();
+            }}
+          >
+            Accept
+          </Button>
+        </Box>
+      ),
+    });
+  };
 
   return (
     <Box>
@@ -176,6 +243,42 @@ export default function Page() {
             {...register('phone_number', { required: 'Phone number is required' })}
           />
           <FormValidation>{errors?.phone_number?.message}</FormValidation>
+        </FormInputField>
+
+        <FormInputField
+          fullWidth
+          error={!!errors.agree_terms}
+        >
+          <FormControlLabel
+            control={
+              <Checkbox
+                {...register('agree_terms', { required: 'You must agree to the Terms and Conditions.' })}
+                color="primary"
+              />
+            }
+            label={
+              <span style={{
+                fontFamily: "'Harmonia Sans Pro', sans-serif",
+                fontWeight: 400,
+                fontSize: "14px",
+                color: "#424242",
+              }}>
+                Please check this box to confirm that you have read, understood, and agree to the{' '}
+                <Button
+                  variant="text"
+                  onClick={openTermsPopup}
+                  sx={{
+                    textDecoration: 'underline', cursor: 'pointer', padding: 0, background: 'transparent', fontFamily: "'Harmonia Sans Pro', sans-serif",
+                    fontWeight: 400,
+                    fontSize: "14px",
+                  }}
+                >
+                  Terms and Conditions
+                </Button>
+              </span>
+            }
+          />
+          <FormValidation>{errors?.agree_terms?.message}</FormValidation>
         </FormInputField>
 
         <Box position="sticky" bottom={0} mt={2}>
