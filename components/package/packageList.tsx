c8c36486843c18c77aca3eb494539213f35c5c68
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2025-02-10 16:30:26 +0530

    add isSelected

--- components/package/packageList.tsx
+++ components/package/packageList.tsx
@@ -4,30 +4,41 @@ import { ShowPrice } from "./showPrice";
 import { PackageDetail } from "./packageDetail";
 
 export const PackageList = () => {
-    const [data, setData] = React.useState<any[]>([]);
+    const [data, setData] = React.useState<any>();
     const [loading, setLoading] = React.useState(true);
-    const [selected, setSelected] = React.useState<any>(null);
-
+    const [selected, setSelected] = React.useState<any>();
 
     React.useEffect(() => {
         getData();
     }, []);
 
     const getData = async () => {
-        setLoading(true);
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/package/packages/`);
+        try {
+            setLoading(true);
+            const response = await fetch(
+                `${process.env.NEXT_PUBLIC_BASE_URL}/package/packages/`,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
 
-        if (response.ok) {
-            const data = await response.json();
-            setData(data);
+            if (response.ok) {
+                const data = await response.json();
+                setData(data);
+                setLoading(false);
+            }
+            setLoading(false);
+        } catch (error) {
             setLoading(false);
         }
-        setLoading(false);
     };
 
 
 
-
+    console.log("data", data)
 
     return (
         <Box pt={2} pb={5}>
@@ -96,38 +107,42 @@ export const PackageList = () => {
                 }
 
                 <Grid2 container spacing={3}>
-                    {data && data.map((item) => (
-                        <Grid2 key={item.id} size={{
-                            xs: 12,
-                            sm: 12,
-                            md: 4,
-                            lg: 4,
-                            xl: 4,
-                        }}>
-                            <Card>
-                                <CardMedia
-                                    sx={{ height: 150 }}
-                                    image={`${process.env.NEXT_PUBLIC_BASE_URL}${item.image}`}
-                                    title={item.name}
-                                />
+                    {data && data.map((item) => {
+                        const isSelected = (item.id == selected?.id)
+                        return (
+                            <Grid2 key={item.id} size={{
+                                xs: 12,
+                                sm: 12,
+                                md: 4,
+                                lg: 4,
+                                xl: 4,
+                            }}>
+                                <Card sx={isSelected ? { bgcolor: "#2755EB", font: "#fff" } : {}}>
+                                    <CardMedia
+                                        sx={{ height: 150 }}
+                                        image={`${process.env.NEXT_PUBLIC_BASE_URL}${item.image}`}
+                                        title={item.name}
+                                    />
 
-                                <CardContent>
-                                    <ShowPrice item={item} />
+                                    <CardContent>
+                                        <ShowPrice item={item} isSelected={isSelected} />
 
-                                    <Typography gutterBottom variant="h5" component="div" fontWeight="bold" color="#2755EB">
-                                        {item.name}
-                                    </Typography>
-                                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                                        {item.description}
-                                    </Typography>
-                                </CardContent>
+                                        <Typography gutterBottom variant="h5" component="div" fontWeight="bold" color={isSelected ? "#fff" : "#2755EB"}>
+                                            {item.name}
+                                        </Typography>
+                                        <Typography variant="body2" color={isSelected ? "#fff" : "#333"}>
+                                            {item.description}
+                                        </Typography>
+                                    </CardContent>
 
-                                <CardActions>
-                                    <Button variant="outlined" onClick={() => setSelected(item)}>Learn More</Button>
-                                </CardActions>
-                            </Card>
-                        </Grid2>
-                    ))}
+                                    <CardActions>
+                                        <Button variant="outlined" onClick={() => setSelected(item)} sx={{ color: isSelected ? "#fff" : "#333", borderColor: isSelected ? "#fff" : "#333" }}>Learn More</Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid2>
+                        )
+                    }
+                    )}
 
                 </Grid2>
 
--- components/package/showPrice.tsx
+++ components/package/showPrice.tsx
@@ -1,18 +1,18 @@
 import { Box, Typography } from "@mui/material"
 
 export const ShowPrice = (props: any) => {
-    const { item } = props
+    const { item, isSelected } = props
 
     if (item.discount_price) {
         return (
             <Box>
-                <Typography sx={{ textDecoration: 'line-through' }}>₦{item.price}</Typography>
-                <Typography>₦{item.discount_price}</Typography>
+                <Typography color={isSelected ? "#fff" : "#333"} sx={{ textDecoration: 'line-through' }}>₦{item.price}</Typography>
+                <Typography color={isSelected ? "#fff" : "#333"}>₦{item.discount_price}</Typography>
             </Box>
         )
     } else {
         return (
-            <Typography>₦{item.price}</Typography>
+            <Typography color={isSelected ? "#fff" : "#333"}>₦{item.price}</Typography>
         )
     }
 }
